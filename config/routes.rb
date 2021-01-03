f242a55c6606f70ed969dba6df5175bf6d42ad61
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-01-02 17:11:17 -0700

    Add logout and dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "home#index"
 
   get '/auth/github/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/dashboard', to: 'dashboard#show'
 end
